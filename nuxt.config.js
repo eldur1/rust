c3a90d6af70ce526bc9e7a6cb949eabf81875ba5
0kira <okira@hotmail.be>
2021-02-17 18:12:14 +0100

    favicon + meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sea-of-rust',
+    title: 'Sea of Rust',
     htmlAttrs: {
       lang: 'fr'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Sea Of Rust est un serveur Rust communautaire rassemblant des passionés de Sea Of Thieves' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
